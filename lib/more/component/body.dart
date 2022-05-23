00e4edd116110f731c1bd58187f6ba36c67a3c20
akiniran123 <bigpuppycat@gmail.com>
2022-05-23 22:09:59 +0700

    Add more onPress
    Viết MoreBulder onPress:' ',

--- lib/more/component/body.dart
+++ lib/more/component/body.dart
@@ -25,11 +25,30 @@ class Body extends StatelessWidget {
                 const SizedBox(
                   height: 16,
                 ),
-                MoreBulder(title: 'Tài khoản', icon: Icons.home),
-                MoreBulder(title: 'Đơn', icon: Icons.home),
-                MoreBulder(title: 'Blog', icon: Icons.home),
-                MoreBulder(title: 'Điều khoản', icon: Icons.home),
-                MoreBulder(title: 'Hỗ trợ', icon: Icons.home),
+                MoreBulder(
+                    title: 'Tài khoản',
+                    icon: Icons.account_circle,
+                    onPress: ''),
+                MoreBulder(
+                  title: 'Đơn',
+                  icon: Icons.receipt,
+                  onPress: '',
+                ),
+                MoreBulder(
+                  title: 'Blog',
+                  icon: Icons.newspaper,
+                  onPress: '',
+                ),
+                MoreBulder(
+                  title: 'Hỗ trợ',
+                  icon: Icons.call,
+                  onPress: '',
+                ),
+                MoreBulder(
+                  title: 'Điều khoản',
+                  icon: Icons.home,
+                  onPress: '',
+                ),
               ],
             ),
           ),
@@ -40,14 +59,16 @@ class Body extends StatelessWidget {
 }
 
 class MoreBulder extends StatelessWidget {
-  MoreBulder({required this.title, required this.icon});
+  MoreBulder({required this.title, required this.icon, required this.onPress});
 
   final IconData icon;
   final String title;
+  final String onPress;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      padding: const EdgeInsets.all(8),
       width: double.infinity,
       height: 48,
       child: ElevatedButton(
@@ -61,7 +82,7 @@ class MoreBulder extends StatelessWidget {
           ),
         ),
         onPressed: () {
-          ;
+          onPress;
         },
         child: RichText(
           // //1 dòng có icon và text
