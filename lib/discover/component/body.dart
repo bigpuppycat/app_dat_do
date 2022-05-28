bb5cc266f3bc604c3a0548d2f44a9615bde210fc
akiniran123 <bigpuppycat@gmail.com>
2022-05-28 13:22:22 +0700

    chỉnh icon
    chỉnh icon

--- lib/discover/component/body.dart
+++ lib/discover/component/body.dart
@@ -10,7 +10,7 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.only(left: 18.0),
+        padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 18),
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
@@ -28,44 +28,52 @@ class Body extends StatelessWidget {
               const SizedBox(
                 height: 10,
               ),
-              Container(
-                // padding: const EdgeInsets.only(
-                //   left: 20.0,
-                // ),
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(right: 2.0, top: 0),
-                      child: Icon(
-                        Icons.person_pin_circle,
-                        size: 18,
-                      ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    // alignment: crossAxisAlignment.to,
+                    // padding: EdgeInsets.only(right: 2.0),
+                    child: Icon(
+                      Icons.person_pin_circle,
+                      size: 18,
                     ),
-                    Container(
-                      padding: EdgeInsets.only(bottom: 24),
-                      child: Column(
-                        children: const <Widget>[
-                          Text(
-                            'Tân Mai, Hà Nội',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 24),
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Tân Mai, Hà Nội',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
-                          SizedBox(height: 4),
-                          Text(
-                            'trong vòng 5km',
-                            style: TextStyle(
-                              fontSize: 14,
-                            ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          'trong vòng 5km',
+                          style: TextStyle(
+                            fontSize: 12,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.grey.shade50),
+                      child: Text('Thay đổi',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(1.0),
+                          ))),
+                ],
               ),
               PopularProducts(),
             ],
